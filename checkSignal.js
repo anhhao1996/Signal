7aa1f81277a56d6069a7a6e17f992ea5ffe3721d
Hao Nguyen <haonguyen@axon.com>
2022-05-30 22:11:54 +0700

    check volume

--- checkSignal.js
+++ checkSignal.js
@@ -20,103 +20,30 @@ async function main(){
         }
     }
 
-    while(true){    
-        for(const coin of symbolsList){
-            const prices = await binance.fetchOHLCV(coin, '1d', undefined, 2)
-            console.log(`\nFetching data for ${coin}`)
-            prices.reverse()
-            var openPrice = prices[0][1]
-            var highPrice = prices[0][2]
-            var lowPrice = prices[0][3]
-            var closePrice = prices[0][4]
-            var previousOpenPrice = prices[1][1]
-            var previousClosePrice = prices[1][4]
-
-            const priceMap = prices.map(price => {
-                return {
-                    OpenTime: moment(price[0]).format("DD/MM/yyyy HH:mm"),
-                    OpenPrice: price[1],
-                    Highest: price[2],
-                    Lowest: price[3],
-                    Current: price[4],
-                    Volume: price[5]
-                }
-            })
-            console.log(priceMap[0])
-
-            if(previousClosePrice * 0.9 >= closePrice){
-                axios.post('https://hooks.slack.com/services/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',
-                    {
-                            "blocks": [
-                                {
-                                    "type": "section",
-                                    "text": {
-                                        "type": "mrkdwn",
-                                        "text": `${coin} current price is <= 10% than the previous price (past 1 day) Current Price: ${closePrice}. Previous Price: ${previousClosePrice}`
-                                    }
-                                }
-                            ]
-                    })
-
-                    axios.post('https://hooks.slack.com/services/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',
-                    {
-                            "blocks": [
-                                {
-                                    "type": "section",
-                                    "text": {
-                                        "type": "mrkdwn",
-                                        "text": `${coin} current price is <= 10% than the previous price (past 1 day) Current Price: ${closePrice}. Previous Price: ${previousClosePrice}`
-                                    }
-                                }
-                            ]
-                    })
+    for(const coin of symbolsList){
+        const prices = await binance.fetchOHLCV(coin, '1d', undefined, 5)
+        prices.reverse()
+        const priceMap = prices.map(price => {
+            return {
+                OpenTime: moment(price[0]).format("DD/MM/yyyy HH:mm"),
+                OpenPrice: price[1],
+                Highest: price[2],
+                Lowest: price[3],
+                Current: price[4],
+                Volume: price[5]
             }
-            
-            if(previousClosePrice < previousOpenPrice &&
-                closePrice > openPrice && 
-                ((openPrice / lowPrice) - 1) * 100 >= 2.99 &&
-                ((closePrice / openPrice) - 1) * 100 <= 1.1 &&
-                ((highPrice / closePrice) - 1) * 100 <= 1.1) {
-                    axios.post('https://hooks.slack.com/services/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',
-                    {
-                            "blocks": [
-                                {
-                                    "type": "section",
-                                    "text": {
-                                        "type": "mrkdwn",
-                                        "text": `Buy Signal for ${coin}`
-                                    }
-                                }
-                            ]
-                    })
-                    axios.post('https://hooks.slack.com/services/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',
-                    {
-                            "blocks": [
-                                {
-                                    "type": "section",
-                                    "text": {
-                                        "type": "mrkdwn",
-                                        "text": `Buy Signal for ${coin}`
-                                    }
-                                }
-                            ]
-                    })
-            }
+        })
+        //Get Average Volume in the last 5 candles
+        var sum = 0;
+        for( var i = 1; i < priceMap.length; i++ ){
+            sum += priceMap[i].Volume
         }
-        console.log("\n Wait 60' to continue!")
-        await delay(60 * 60 * 1000);
+        const averageVolume = sum/priceMap.length
+
+        if((priceMap[1].Volume >= averageVolume * 1.4) && (priceMap[1].Volume <= averageVolume * 1.7)){
+            console.log(`Massive Volume for: ${coin}`)
+        }
     }
 }
 
-async function viresState(){
-    var viresState = await axios.get("https://api.vires.finance/state")
-    var viresResponse = viresState.data.markets
-    viresResponse.forEach(coin =>
-        {
-            if(coin.totalSupply > coin.totalDebt && coin.totalSupplyUsd > coin.totalDebtUsd){
-                console.log(`Able to withdraw ${coin.name} with APY ${(coin.supplyApy * 100).toFixed(2)}%`)
-            }
-        })
-    console.log(viresResponse)
-}
-viresState()
+main()
